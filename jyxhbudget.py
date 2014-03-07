c4e4433a2d39d6d3e7f07200fff9c9a1c7bede46
alkjash <alkjash876@gmail.com>
2014-03-07 12:56:45 -0500

    Moved to right file

--- jyxhbudget.py
+++ jyxhbudget.py
@@ -5,15 +5,15 @@ import sys
 from gsp import GSP
 from util import argmax_index
 
-class Jyxhbb:
-    """Balanced bidding agent"""
+class Jyxhbudget:
+    """Budget-aware balanced bidding agent"""
     def __init__(self, id, value, budget):
         self.id = id
         self.value = value
         self.budget = budget
 
     def initial_bid(self, reserve):
-        return self.value / 2
+        return self.value * 2/3
 
 
     def slot_info(self, t, history, reserve):
@@ -43,16 +43,26 @@ class Jyxhbb:
 
     def expected_utils(self, t, history, reserve):
         """
-        Figure out the expected utility of bidding such that we win each
-        slot, assuming that everyone else keeps their bids constant from
-        the previous round.
-
-        returns a list of utilities per slot.
+        Project expected utilities for the rest of the rounds given that everyone's bids
+        stay constant and we bid for each given slot; takes into account budget constraints.
         """
-        # TODO: Fill this in
-        utilities = []   # Change this
+        # Compute the amount needed per slot
+        info = self.slot_info(t, history, reserve)
 
+        # Compute the utility of winning the slot: pay the min_bid because
+        # that's the bid of the next highest bid
+        clicks = history.round(t-1).clicks
 
+        def calc_util(i):
+            (s, min_bid, max_bid) = info[i]
+
+            util = clicks[i] * (self.value - min_bid)
+            if self.budget < min_bid * clicks[i] * (48 - t) and min_bid != 0: # Unsustainable
+                return util * self.budget / (min_bid * clicks[i] * (48 - t)) 
+            else:
+                return util
+
+        utilities = map(calc_util, range(len(clicks)))
         return utilities
 
     def target_slot(self, t, history, reserve):
@@ -63,9 +73,18 @@ class Jyxhbb:
         the other-agent bid for that slot in the last round.  If slot_id = 0,
         max_bid is min_bid * 2
         """
-        i =  argmax_index(self.expected_utils(t, history, reserve))
+        i = argmax_index(self.expected_utils(t, history, reserve))
+
         info = self.slot_info(t, history, reserve)
         return info[i]
+
+    def update_budget(self, t, history):
+        agents = history.round(t-1).occupants
+        if self.id in agents:
+            slot = agents.index(self.id)
+            self.budget -= history.round(t-1).slot_payments[slot]
+
+        return self.budget
 
     def bid(self, t, history, reserve):
         # The Balanced bidding strategy (BB) is the strategy for a player j that, given
@@ -78,11 +97,16 @@ class Jyxhbb:
         # (p_x is the price/click in slot x)
         # If s*_j is the top slot, bid the value v_j
 
+        self.update_budget(t, history)
+
         prev_round = history.round(t-1)
         (slot, min_bid, max_bid) = self.target_slot(t, history, reserve)
 
-        # TODO: Fill this in.
-        bid = 0  # change this
+        if self.value < min_bid or slot == 0: # Not expecting to win
+            bid = self.value
+        else: # Bid regularly
+            clicks = prev_round.clicks
+            bid = self.value - ((clicks[slot] * (self.value - min_bid)) / clicks[slot-1])
 
         return bid
 
